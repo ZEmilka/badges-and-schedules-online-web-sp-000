ee2cdeafdb5a9cc9067d76e6b9b51b8e8b7bb44e
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-16 16:52:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,5 @@ end
 
 def batch_badge_creator(attendees)
   attendees=["Edsger","Ada","Charles","Alan","Linus","Matz"]
-  return"Hello, my name is #{name}."
+  return"Hello, my name is #{name}.each."
 end
